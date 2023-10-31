b79bc594795c0236623994adf1726b1152811224
손원서 <141789529+sws1021@users.noreply.github.com>
2023-10-31 14:33:58 +0900

    Update script_setting.sh

--- ml/script_setting.sh
+++ ml/script_setting.sh
@@ -19,6 +19,4 @@ wget -q https://github.com/IDEA-Research/GroundingDINO/releases/download/v0.1.0-
 cd ..
 echo "Grounding DINO check"
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1826V4X63UFaN1ElTN17kSiwRqIMRiQ-c' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1826V4X63UFaN1ElTN17kSiwRqIMRiQ-c" -O resnet50_final_dish.pth && rm -rf /tmp/cookies.txt
-
 python3 real_API.py
