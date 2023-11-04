4c9cfbbb3a006e02ce0835e1aabc569440b70071
user <user@DESKTOP-R1BI8AH>
2023-11-04 12:39:39 +0900

    zzz

--- ReGoProject/src/main/java/com/smhrd/repository/r_followRepository.java
+++ ReGoProject/src/main/java/com/smhrd/repository/r_followRepository.java
@@ -1,5 +1,7 @@
 package com.smhrd.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +9,13 @@ import com.smhrd.entity.r_follow;
 
 @Repository
 public interface r_followRepository extends JpaRepository<r_follow, String> {
+	
+	public List<r_follow> findByFollowee(String followee);
+	public List<r_follow> findByFollower(String follower);
+	public void deleteByFollowerAndFollowee(String follower, String followee);
+//	public void saveByFollowerAndFollowee(String follower, String followee);
+	public List<r_follow> findByFollowerAndFollowee(String follower, String followee);
+	
 
 
 
