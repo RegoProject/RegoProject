dbb081eb4af4c206d7edda6fa21a23c63d5693b4
smhrd <smhrd@DESKTOP-SN783QO>
2023-11-08 11:01:31 +0900

    ㄱㄹ

--- ReGoProject/src/main/java/com/smhrd/controller/MyPageController.java
+++ ReGoProject/src/main/java/com/smhrd/controller/MyPageController.java
@@ -156,7 +156,7 @@ public class MyPageController {
 
 			// 3. 어디에 저장할지 
 			
-			String savePath = "/uploadedImage/";
+			String savePath = "imagePath/uploadedImag/";
 			System.out.println(savePath);
 			
 		
